22527f16ba5bce5bf8b02de6cf2b81e1cb6d6ef6
ChrisCameron12 <cameronchris535@gmail.com>
2025-06-02 15:06:04 +1200

    minor change

--- src/unlayerSampleJSON.js
+++ src/unlayerSampleJSON.js
@@ -15,59 +15,7 @@ export const unlayerSampleJSON = {
           {
             id: "Lq0c-hF1hz",
             contents: [
-              {
-                id: "k6I_VDAZ8F",
-                type: "image",
-                values: {
-                  containerPadding: "10px",
-                  anchor: "",
-                  src: {
-                    author: {
-                      name: "Zachtleven",
-                      url: "https://pixabay.com/photos/animal-bulldog-dog-canine-mammal-9417081/"
-                    },
-                    id: 30441519,
-                    platform: {
-                      name: "Pixabay",
-                      url: "https://pixabay.com"
-                    },
-                    url: "https://assets.unlayer.com/projects/274653/1748736277078-11539",
-                    width: 640,
-                    height: 427,
-                    preview: {
-                      url: "https://pixabay.com/get/g3a7de9d2d6db393a93e4da51f5465d8388561bbbb31a7d767cee742b8b26d21ae88475757d2b86c91b53565984d6fcab56bf97a5511e85018806f44bdb025a5e_640.jpg",
-                      width: 640,
-                      height: 427
-                    },
-                    filename: "11539",
-                    contentType: "image/jpeg",
-                    size: 42947,
-                    dynamic: true
-                  },
-                  textAlign: "center",
-                  altText: "",
-                  action: {
-                    name: "web",
-                    values: {
-                      href: "",
-                      target: "_blank"
-                    }
-                  },
-                  displayCondition: null,
-                  _styleGuide: null,
-                  _meta: {
-                    htmlID: "u_content_image_1",
-                    htmlClassNames: "u_content_image",
-                    description: ""
-                  },
-                  selectable: true,
-                  draggable: true,
-                  duplicatable: true,
-                  deletable: true,
-                  hideable: true,
-                  locked: false
-                }
-              }
+              
             ],
             values: {
               border: {},
@@ -214,56 +162,7 @@ export const unlayerSampleJSON = {
               locked: false
             }
           },
-          {
-            id: "R4_rAdOBNB",
-            contents: [
-              {
-                id: "9cTkYNeDxR",
-                type: "text",
-                values: {
-                  containerPadding: "10px",
-                  anchor: "",
-                  fontSize: "14px",
-                  textAlign: "left",
-                  lineHeight: "140%",
-                  linkStyle: {
-                    inherit: true,
-                    linkColor: "#0000ee",
-                    linkHoverColor: "#0000ee",
-                    linkUnderline: true,
-                    linkHoverUnderline: true
-                  },
-                  displayCondition: null,
-                  _styleGuide: null,
-                  _meta: {
-                    htmlID: "u_content_text_3",
-                    htmlClassNames: "u_content_text",
-                    description: ""
-                  },
-                  selectable: true,
-                  draggable: true,
-                  duplicatable: true,
-                  deletable: true,
-                  hideable: true,
-                  locked: false,
-                  text: `<p style="line-height: 140%;">This is a new Text block. Change the text.</p>`,
-                  _languages: {}
-                }
-              }
-            ],
-            values: {
-              backgroundColor: "",
-              padding: "0px",
-              border: {},
-              borderRadius: "0px",
-              _meta: {
-                htmlID: "u_column_4",
-                htmlClassNames: "u_column"
-              },
-              deletable: true,
-              locked: false
-            }
-          }
+     
         ],
         values: {
           displayCondition: null,
